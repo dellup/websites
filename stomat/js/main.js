0d844582548e62872189c833d1f661d6fd9bd03d
Danil Zenin <zenindanil2000@gmail.com>
2024-10-05 21:35:16 +0300

    add site

---
+++ stomat/js/main.js
@@ -0,0 +1,79 @@
+$('.main__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
+$('.services__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
+const video = document.querySelector('.video__item');
+const playButton = document.querySelector('.video__button');
+
+playButton.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
+
+document.querySelector('.video__button').addEventListener('mouseenter', () => {
+    if (!video.paused) {
+        playButton.style.opacity = '1';
+    }
+});
+
+document.querySelector('.video__button').addEventListener('mouseleave', () => {
+    if (!video.paused) {
+        playButton.style.opacity = '0';
+    }
+});
+
+$(function(){
+      $('.raboti__images').beforeAfter({
+            movable: true,
+            clickMove: true,
+            alwaysShow: true,
+            position: 50,
+            opacity: 0.4,
+            activeOpacity: 1,
+            hoverOpacity: 0.8,
+
+            // slider colors
+            separatorColor: '#ffffff',
+            bulletColor: '#ffffff',
+            arrowColor: '#333333',
+      });
+});
+
+$('.raboti__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    draggable: false,
+});
+$('.sertif__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    draggable: true,
+});
+$('.otzivi__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    draggable: true,
+});
