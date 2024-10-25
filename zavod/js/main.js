848fb73712a80c24d480268f3ab434262e031b87
Danil Zenin <zenindanil2000@gmail.com>
2024-10-25 15:57:48 +0300

    add content

---
+++ zavod/js/main.js
@@ -0,0 +1,10 @@
+$('.main__slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+})
