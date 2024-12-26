7fd53ecaf269669c9f2250b3ff78bced95e9c591
Danil Zenin <zenindanil2000@gmail.com>
2024-12-26 21:46:20 +0300

    fix fensol

--- rensol/js/dilegramm.js
+++ rensol/js/dilegramm.js
@@ -2,9 +2,18 @@ $('.slider__block').slick({
     infinite: true,
     centerMode: true,
     centerPadding: '150px',
-    slidesToShow: 4,
+    slidesToShow: 5,
     arrows: false,
     responsive: [
+        {
+            breakpoint: 1500,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '80px',
+              slidesToShow: 4
+            }
+        },
         {
           breakpoint: 1100,
           settings: {
