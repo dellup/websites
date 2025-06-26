6ad3ea9c50f0a876fe3efc3bfd73d9f5be457ea7
Danil Zenin <zenindanil2000@gmail.com>
2025-06-26 23:49:30 +0300

    add kater site

---
+++ kater/js/main.js
@@ -0,0 +1,9 @@
+$('.kater__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true
+});
